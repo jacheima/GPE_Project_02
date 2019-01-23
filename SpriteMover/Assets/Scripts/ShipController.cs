d18bc7ca7364d1d2655826323b2ce44bd34f30e3
Jacquelynne Heiman <jacheima@uat.edu>
2019-01-23 10:16:48 -0700

    I made a bullet prefab and started to code for the player being able to shoot. I did make a bullet controler C# file that causes the bullet to move in a forward direction when the player shoots it. I also put in a spawn location but I have not gotten the shooting to work yet.

--- SpriteMover/Assets/Scripts/ShipController.cs
+++ SpriteMover/Assets/Scripts/ShipController.cs
@@ -7,6 +7,8 @@ public class ShipController : MonoBehaviour {
     public Vector2 center = new Vector2(0.0f, 0.0f);
     private Transform ship;
     public float speed;
+    public GameObject bullet;
+    public Transform shotSpawn;
 
 
 	// Use this for initialization
@@ -16,63 +18,28 @@ public class ShipController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) //if the user presses left or right shift
+        
+	    
+	}
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) // if the player presses the left arrow or the a key
         {
-	        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) //and if the user presses the left arrow or a key
-	        {
-	            Vector2 pos = transform.position; //set the current position to a vector variable
-	            pos.x--; //decrement the position on the x axis by 1 moving the sprite left one sprite
-	            transform.position = pos; //set the new position to the variable
-                
-	        }
-
-	        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))  //if the user presses right arrow or d key
-            {
-                Vector2 pos = transform.position; //set the current position to a vector variable
-                pos.x++; //increment the position by 1 on the x axis moving the sprite to the right one unit
-                transform.position = pos; //set the new position to the variable
-            }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))  //if the user presses the up arrow or the w key
-            {
-                Vector2 pos = transform.position;  //set the current position to a vector variable
-                pos.y++; //increment the position by 1 on the y axis moving the sprite up one unit
-                transform.position = pos;
-
-            }
-            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))  //if the user presses the down arrow or the s key
-            {
-                Vector2 pos = transform.position; //set the current position to a vector variable
-                pos.y--;  //decrement the position by 1 on the y axis moving the player down one unit
-                transform.position = pos;
-
-            }
-
+            transform.Rotate(new Vector3(0f, 0f, 90f) * Time.deltaTime);
         }
-	    else //Otherwise
-	    {
-	        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) // if the player presses the left arrow or the a key
-	        {
-                ship.position = ship.position + (Vector3.left * speed); //the sprite position and add the vector left times the speed to move the sprite
-            }
 
-	        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))  // if the player presses the right arrow or d key
-	        {
-                ship.position = ship.position + (Vector3.right * speed);  //the sprite position and add the vector right times the speed to move the sprite
-            }
-            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))  //if the player presses the up arrow or the w key
-            {
-                ship.position = ship.position + (Vector3.up * speed);  //the sprite position and add the vector up times the speed to move the sprite
-            }
-            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))  //if the player presses the down arrow or the s key
-            {
-                ship.position = ship.position + (Vector3.down * speed);  //the sprite position and add the vector down times the speed to move the sprite
-            }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))  // if the player presses the right arrow or d key
+        {
+            transform.Rotate(new Vector3(0f, 0f, -90f) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))  //if the player presses the up arrow or the w key
+        {
+            transform.position += transform.up * speed;
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = center;
+            Instantiate(bullet, shotSpawn.position, shotSpawn.rotation);
 
         }
 
@@ -80,7 +47,6 @@ public class ShipController : MonoBehaviour {
         {
             gameObject.SetActive(false);
         }
-	    
-	}
+    }
 }
 
